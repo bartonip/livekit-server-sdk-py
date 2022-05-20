b9b11fc829f91edf8ceb3e1f00392b311f873129
bartonip <barton.ip@icloud.com>
2022-05-20 05:20:09 +0000

    Version bump to 0.1.1

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 # The version of this tool is based on the following steps:
 # https://packaging.python.org/guides/single-sourcing-package-version/
-VERSION = "0.1.0"
+VERSION = "0.1.1"
 
 
 setup(
